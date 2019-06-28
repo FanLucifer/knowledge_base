05a7dd2b280b9c9e8f98be704590eceda6601980
Faye Fan <fayefan@augmentum.com.cn>
2019-06-28 17:50:13 +0800

    [RubyTutorial]

--- Ruby/Rails/blog/app/controllers/articles_controller.rb
+++ Ruby/Rails/blog/app/controllers/articles_controller.rb
@@ -22,6 +22,28 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to articles_path
+  end
+
   private
   def article_params
     params.require(:article).permit(:title, :text)
